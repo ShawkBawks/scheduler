0510e5f2cc61865a573016d8f952fd19a573c9e4
Logan Roberts <lroberts77@gmail.com>
2019-11-06 13:48:42 -0800

    Refactored code and added comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,18 +3,18 @@ import React from "react";
 import "components/Button.scss";
 const classNames = require('classnames');
 
-
+//Button Form
 
 export default function Button(props) {
-   let buttonClass = classNames ("button",{
+   let buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
    });
 
    return (<button
-    className={buttonClass}
-    onClick={props.onClick}
-    disabled={props.disabled}
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
    >{props.children}</button>
    );
 }
