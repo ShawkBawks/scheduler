9d6dc6e8add9a172456a727c9492874ebc0d8055
Logan Roberts <lroberts77@gmail.com>
2019-11-06 13:43:00 -0800

    Refactored code and added comments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,37 +2,37 @@ import React from "react";
 import Appointment from "components/Appointment/index.js";
 import DayList from 'components/DayList.js';
 import "components/Application.scss";
-import  useApplicationData from "hooks/useApplicationData.js"
+import useApplicationData from "hooks/useApplicationData.js"
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
 
 
 export default function Application(props) {
+//Hook Data
   const {
     state,
     setDay,
     bookInterview,
     cancelInterview
   } = useApplicationData();
-  
-const appointments = 
-getAppointmentsForDay(state, state.day).map(appointment => {
+//Appointment Card Element
+  const appointments =
+    getAppointmentsForDay(state, state.day).map(appointment => {
 
-  return <Appointment
-    key={appointment.id}
-    id={appointment.id}
-    time={appointment.time}
-    interview={getInterview(state, appointment.interview)}
-    interviewers={getInterviewersForDay(state, state.day)}
-    bookInterview={bookInterview}
-    cancelInterview={cancelInterview}
+      return <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={getInterview(state, appointment.interview)}
+        interviewers={getInterviewersForDay(state, state.day)}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
 
-  />
-})
+      />
+    })
 
-  
-  // const setDays = days => setState({ ...state, days });
 
+//Application Card Element
   return (
     <main className="layout">
       <section className="sidebar">
@@ -54,12 +54,11 @@ getAppointmentsForDay(state, state.day).map(appointment => {
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />      </section>
-        {/* appointments object goes below */}
       <section className="schedule">
         <section className="schedule">
-       {appointments}
-        <Appointment key="last" time="5pm" />
-      </section>
+          {appointments}
+          <Appointment key="last" time="5pm" />
+        </section>
       </section>
     </main>
   );
