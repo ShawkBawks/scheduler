e77e7e19ef00446f412b12691138aa2ff6aa4b74
Logan Roberts <lroberts77@gmail.com>
2019-11-06 13:49:47 -0800

    Refactored code and added comments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,39 +1,39 @@
+//Function for Getting Individual Appointments for Each Day
 export function getAppointmentsForDay(state, day) {
-  const appointments=[];
+  const appointments = [];
   const filteredDays = state.days.find(dayCheck => dayCheck.name === day)
 
-  if(!filteredDays) return appointments;
-  
+  if (!filteredDays) return appointments;
+
   for (let id of filteredDays.appointments) {
 
-    //id-1  because it needs to count from 0 rather than the human ordering starting at 1
     appointments.push(state.appointments[id])
   }
-    return appointments;
+  return appointments;
 }
 
-
+//Function for Displaying Interviewers Available For Each Day
 export function getInterviewersForDay(state, day) {
 
-  const interviewers=[];
+  const interviewers = [];
   const filteredDay = state.days.find(dayCheck => dayCheck.name === day)
-  if(!filteredDay) return interviewers;
+  if (!filteredDay) return interviewers;
   for (let id of filteredDay.interviewers) {
-  
-    
+
+
     interviewers.push(state.interviewers[id])
   }
-    return interviewers;
+  return interviewers;
 }
 
-
+//Displays Appointment Data With Student and Interviewer Names
 export const getInterview = (state, interview) => {
-  if (!interview){
+  if (!interview) {
     return null
   } else {
     let student = interview.student
     let interviewer = state.interviewers[interview.interviewer]
-    let obj = {student, interviewer}
+    let obj = { student, interviewer }
     return obj
   }
 }
